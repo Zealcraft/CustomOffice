65b2f4f90188729da2a778d59d3c399dc73d519a
Simon Bastien-Filiatrault <root@gopoi.net>
2013-08-19 02:43:45 -0400

    plugin.yml now follows build version.

--- src/main/java/net/zealcraft/trait/CustomOfficerTrait.java
+++ src/main/java/net/zealcraft/trait/CustomOfficerTrait.java
@@ -1,5 +1,38 @@
 package net.zealcraft.trait;
 
+/*
+ * #%L
+ * CustomOffice
+ * %%
+ * Copyright (C) 2013 Shan Meunier Bernard <shan@sbernard.info>, Simon Bastien-Filiatrault <root@gopoi.net>
+ * %%
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+
 import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.util.DataKey;
 import net.citizensnpcs.trait.Toggleable;
@@ -7,13 +40,6 @@ import net.zealcraft.CustomOffice;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tchoutchawn
- * Date: 8/18/13
- * Time: 9:02 PM
- * To change this template use File | Settings | File Templates.
- */
 public class CustomOfficerTrait extends Trait {
     private CustomOffice plugin=null;
 
